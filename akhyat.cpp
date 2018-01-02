8d04d5e8cb6e01f9d501496e19b53a78a57e4974
akhyat555@gmail.com <nabilaholic>
2018-01-02 11:14:00 +0800

    membuat matrix R * W

--- akhyat.cpp
+++ akhyat.cpp
@@ -2,9 +2,9 @@
 #include <cmath>
 using namespace std;
 
-
 int main(){
 	cout.setf(ios::fixed); cout.setf(ios::showpoint); cout.precision(4);
+
 	double a1c[5]={4,4,5,3,3}, a2c[5]={3,3,4,2,3}, a3c[5]={5,4,2,2,2};
 
 	cout << "MATRIX X :"<<endl;
@@ -23,7 +23,7 @@ int main(){
 		cout << a3c[biawak3]<<" | ";
 		biawak3++;
 	}while(biawak3<5);cout<<endl;
-	
+
 	double mc1=max(a1c[0],max(a2c[0],a3c[0])),
 	mc2=max(a1c[1],max(a2c[1],a3c[1])),
 	mc3=max(a1c[2],max(a2c[2],a3c[2])),
@@ -33,7 +33,7 @@ int main(){
 	double Ra1c[5]={ (a1c[0]/mc1),(a1c[1]/mc2),(a1c[2]/mc3),(a1c[3]/mc4),(a1c[4]/mc5) };
 	double Ra2c[5]={ (a2c[0]/mc1),(a2c[1]/mc2),(a2c[2]/mc3),(a2c[3]/mc4),(a2c[4]/mc5) };
 	double Ra3c[5]={ (a3c[0]/mc1),(a3c[1]/mc2),(a3c[2]/mc3),(a3c[3]/mc4),(a3c[4]/mc5) };
-
+	
 		cout << endl<<endl;
 		cout << "MATRIX R :"<<endl;
 	int a=0;
@@ -51,8 +51,31 @@ int main(){
 		cout << Ra3c[c]<<" | ";
 		c++;
 	}while(c<5);cout<<endl;
-	
+
 	double w1=5, w2=3, w3=4, w4=4, w5=2;
 	cout << endl << "BOBOT W :"<<endl<< w1<<" | "<< w2<<" | "<< w3<<" | "<< w4<<" | "<< w5<<" | "<<endl;
 
+	double RWa1c[5]={ (Ra1c[0]*w1),(Ra1c[1]*w2),(Ra1c[2]*w3),(Ra1c[3]*w4),(Ra1c[4]*w5) };
+	double RWa2c[5]={ (Ra2c[0]*w1),(Ra2c[1]*w2),(Ra2c[2]*w3),(Ra2c[3]*w4),(Ra2c[4]*w5) };
+	double RWa3c[5]={ (Ra3c[0]*w1),(Ra3c[1]*w2),(Ra3c[2]*w3),(Ra3c[3]*w4),(Ra3c[4]*w5) };
 
+	cout << endl<<endl;
+		cout << "MATRIX R * W :"<<endl;
+	int putri1=0;
+	do{
+		cout << RWa1c[putri1]<<" | ";
+		putri1++;
+	}while(putri1<5);cout<<endl;
+		int putri2=0;
+	do{
+		cout << RWa2c[putri2]<<" | ";
+		putri2++;
+	}while(putri2<5);cout<<endl;
+		int putri3=0;
+	do{
+		cout << RWa3c[putri3]<<" | ";
+		putri3++;
+	}while(putri3<5);cout<<endl;
+	
+}
+	
